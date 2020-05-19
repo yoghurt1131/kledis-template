d060a65ae88a4a188907fc0e17787d4061f075ec
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-05-19 14:51:27 +0900

    update

--- src/main/kotlin/dev/yoghurt1131/kledistemplate/core/config/KLedisConfiguration.kt
+++ src/main/kotlin/dev/yoghurt1131/kledistemplate/core/config/KLedisConfiguration.kt
@@ -1,15 +1,11 @@
 package dev.yoghurt1131.kledistemplate.core.config
 
 import dev.yoghurt1131.kledistemplate.core.accessor.KLedisTemplateBuilder
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(name =["kledis.enable"], matchIfMissing = true)
